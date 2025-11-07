214622867e8efa82f5c2c5e8fe132a31d6876fc4
Macbook Pro 2020 <admin@MacBook-Pro.local>
2025-11-07 21:12:56 +0100

    Fix: remove duplicate logic in 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,18 +4,4 @@ const num = parseInt(arg);
 if (isNaN(num)) {
   console.log("Not a number");
 } else {
-  console.log("My number: " + num);
-}
-if (isNaN(num)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${num}`);
-}
-const arg = process.argv[2];
-const num = parseInt(arg, 10);
-
-if (isNaN(num)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${num}`);
-}
+  
